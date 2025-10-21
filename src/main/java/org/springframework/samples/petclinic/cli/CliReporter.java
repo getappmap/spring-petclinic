c9f211b5257353399e210ebe02c49615018c8c25
Rafał Rzepecki <divided.mind@gmail.com>
2025-10-21 15:24:01 +0200

    Appmap config file generating large AppMaps

--- src/main/java/org/springframework/samples/petclinic/cli/CliReporter.java
+++ src/main/java/org/springframework/samples/petclinic/cli/CliReporter.java
@@ -2,7 +2,6 @@ package org.springframework.samples.petclinic.cli;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.samples.petclinic.vet.Vet;
 import org.springframework.samples.petclinic.vet.VetRepository;
